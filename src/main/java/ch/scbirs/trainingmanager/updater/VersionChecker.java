fba9f47db79198957c0f28df938f02487f53974c
Tiim <tim.bachmann@cad.ch>
2014-07-09 22:09:09 +0200

    Add methods to perform a version check on the updater.

--- src/main/java/ch/scbirs/trainingmanager/updater/VersionChecker.java
+++ src/main/java/ch/scbirs/trainingmanager/updater/VersionChecker.java
@@ -1,6 +1,7 @@
 package ch.scbirs.trainingmanager.updater;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -10,6 +11,8 @@ import java.net.URLConnection;
  */
 public class VersionChecker {
     private static final String REMOTE_VERSION_URL = "https://dl.dropboxusercontent.com/u/49598155/sm/version.txt";
+    private static final String REMOTE_UPDATER_VERSION_URL = "https://dl.dropboxusercontent.com/u/49598155/sm/updaterVersion.txt";
+    private static final String LOCAL_UPDATER_VERSION_URL = "updaterVersion.txt";
     private static Version currentVersion = null;
     private static Version remoteVersion = null;
 
@@ -46,16 +49,7 @@ public class VersionChecker {
     public static void reloadRemoteVersion() {
         Version v;
         try {
-            URL remote = new URL(REMOTE_VERSION_URL);
-            URLConnection conn = remote.openConnection();
-            BufferedInputStream is = new BufferedInputStream(conn.getInputStream());
-            byte[] contents = new byte[20];
-            int bytesRead;
-            String str = "";
-            while ((bytesRead = is.read(contents)) != -1) {
-                str += new String(contents, 0, bytesRead);
-            }
-            v = new Version(str);
+            v = new Version(downloadString(REMOTE_VERSION_URL));
         } catch (IOException e) {
             e.printStackTrace();
             v = new Version();
@@ -63,9 +57,44 @@ public class VersionChecker {
         remoteVersion = v;
     }
 
+    public static boolean isNewUpdaterVersionAvailable() {
+        try {
+            Version remote = new Version(downloadString(REMOTE_UPDATER_VERSION_URL));
+            Version local = new Version(readString(LOCAL_UPDATER_VERSION_URL));
+            return local.compareTo(remote) < 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public static boolean isNewVersionAvailable() {
         Version current = getCurrentVersion();
         Version remote = getRemoteVersion();
         return current.compareTo(remote) < 0;
+    }
+
+    private static String downloadString(String url) throws IOException {
+        URL remote = new URL(url);
+        URLConnection conn = remote.openConnection();
+        BufferedInputStream is = new BufferedInputStream(conn.getInputStream());
+        byte[] contents = new byte[20];
+        int bytesRead;
+        String str = "";
+        while ((bytesRead = is.read(contents)) != -1) {
+            str += new String(contents, 0, bytesRead);
+        }
+        return str;
+    }
+
+    private static String readString(String file) throws IOException {
+        BufferedInputStream is = new BufferedInputStream(new FileInputStream(file));
+        byte[] contents = new byte[20];
+        int bytesRead;
+        String str = "";
+        while ((bytesRead = is.read(contents)) != -1) {
+            str += new String(contents, 0, bytesRead);
+        }
+        return str;
     }
 }
