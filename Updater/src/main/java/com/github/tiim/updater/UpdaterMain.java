8dc99d14e8649929d997d0690712c1a697b69d1a
Tiim <tim.bachmann@cad.ch>
2014-07-12 18:09:31 +0200

    Modified build script and updater to allow download of updated libraries.
    The updater downloads now a zip file, containing the main application and its dependencies and extracts them.

--- Updater/src/main/java/com/github/tiim/updater/UpdaterMain.java
+++ Updater/src/main/java/com/github/tiim/updater/UpdaterMain.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Error codes:
@@ -18,21 +20,21 @@ import java.net.URLConnection;
  * @author Tim
  * @since 07 - 2014
  */
-public class UpdaterMain {
-    public static void main(String[] args) {
+public final class UpdaterMain {
+    public static void main(final String[] args) {
         if (args.length != 2) {
             abort("Couldn't download the updated version. Please do it manually.\n" +
                     "Error Code: 1");
         }
         JOptionPane.showMessageDialog(null, "Updater", "sdf", JOptionPane.WARNING_MESSAGE);
         System.out.println(System.getProperty("user.dir"));
-        String url = args[0];
-        String exec = args[1];
+        final String url = args[0];
+        final String exec = args[1];
 
         try {
-            download(url);
-        } catch (IOException e) {
-            StringWriter w = new StringWriter();
+            downloadAndExtract(url);
+        } catch (final IOException e) {
+            final StringWriter w = new StringWriter();
             e.printStackTrace(new PrintWriter(w));
             abort("Couldn't download the updated version. Please do it manually.\n" +
                     "Error Code: 2\n" +
@@ -42,7 +44,7 @@ public class UpdaterMain {
 
         try {
             execute(exec);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             abort("Couldn't restart the updated version. Please do it manually.\n" +
                     "Error Code: 3\n" +
                     e.getMessage());
@@ -51,31 +53,54 @@ public class UpdaterMain {
 
     }
 
-    private static void execute(String exec) throws IOException {
-        Process p = Runtime.getRuntime().exec(exec);
+    private static void execute(final String exec) throws IOException {
+        final Process p = Runtime.getRuntime().exec(exec);
         System.out.println("Execute " + exec);
         try {
             System.out.println(p.waitFor());
-        } catch (InterruptedException ignored) {
+        } catch (final InterruptedException ignored) {
         }
     }
 
-    private static void download(String url) throws IOException {
-        URL u = new URL(url);
-        String fileName = url.substring(url.lastIndexOf('/') + 1);
-        URLConnection con = u.openConnection();
-        System.out.println("Downloading " + url + " to " + fileName);
-        try (InputStream is = con.getInputStream(); FileOutputStream fos = new FileOutputStream(fileName)) {
-            byte[] data = new byte[1024];
-            int n;
-            while ((n = is.read(data)) != -1) {
-                fos.write(data, 0, n);
+    private static void downloadAndExtract(final String url) throws IOException {
+        final URL u = new URL(url);
+        final URLConnection con = u.openConnection();
+        System.out.println("Downloading " + url);
+
+        final File outputFolder = new File(".");
+
+        try (ZipInputStream zis = new ZipInputStream(con.getInputStream())) {
+            ZipEntry e = zis.getNextEntry();
+            while (e != null) {
+                if (!e.isDirectory()) {
+                    final String zipFileName = e.getName();
+                    final File zipNewFile = new File(outputFolder, zipFileName);
+                    System.out.println("Extracting " + zipFileName + " to " + zipNewFile);
+                    if (!new File(zipNewFile.getParent()).mkdirs()) {
+                        System.err.println("Could not make folder " + zipNewFile.getParent());
+                    }
+                    try (FileOutputStream fos = new FileOutputStream(zipNewFile)) {
+                        int len;
+                        final byte[] buffer = new byte[1024];
+                        while ((len = zis.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                }
+                e = zis.getNextEntry();
             }
         }
+//        try (InputStream is = con.getInputStream(); FileOutputStream fos = new FileOutputStream(fileName)) {
+//            final byte[] data = new byte[1024];
+//            int n;
+//            while ((n = is.read(data)) != -1) {
+//                fos.write(data, 0, n);
+//            }
+//        }
     }
 
 
-    public static void abort(String message) {
+    private static void abort(final String message) {
         System.err.println(message);
         JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
         System.exit(1);
