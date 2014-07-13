ddb94e69d2b448c46df15100acbb0a978853d6cd
Tiim <tim.bachmann@cad.ch>
2014-07-13 21:44:10 +0200

    Updater now shows progress.

--- Updater/src/main/java/com/github/tiim/updater/UpdaterMain.java
+++ Updater/src/main/java/com/github/tiim/updater/UpdaterMain.java
@@ -21,13 +21,22 @@ import java.util.zip.ZipInputStream;
  * @since 07 - 2014
  */
 public final class UpdaterMain {
-    public static void main(final String[] args) {
+
+    private static final ProgressDialog dialog = new ProgressDialog();
+
+    public static void main(final String[] args) throws InterruptedException {
+        if (args.length == 1 && args[0].equals("gui")) {
+            Thread.sleep(10000);
+            System.exit(0);
+        }
+
+
         if (args.length != 2) {
-            abort("Couldn't download the updated version. Please do it manually.\n" +
+            dialog.message("Couldn't download the updated version. Please do it manually.\n" +
                     "Error Code: 1");
         }
-        JOptionPane.showMessageDialog(null, "Updater", "sdf", JOptionPane.WARNING_MESSAGE);
-        System.out.println(System.getProperty("user.dir"));
+
+        dialog.message(System.getProperty("user.dir"));
         final String url = args[0];
         final String exec = args[1];
 
@@ -36,7 +45,7 @@ public final class UpdaterMain {
         } catch (final IOException e) {
             final StringWriter w = new StringWriter();
             e.printStackTrace(new PrintWriter(w));
-            abort("Couldn't download the updated version. Please do it manually.\n" +
+            dialog.message("Couldn't download the updated version. Please do it manually.\n" +
                     "Error Code: 2\n" +
                     w.toString());
             e.printStackTrace();
@@ -45,39 +54,39 @@ public final class UpdaterMain {
         try {
             execute(exec);
         } catch (final IOException e) {
-            abort("Couldn't restart the updated version. Please do it manually.\n" +
+            dialog.message("Couldn't restart the updated version. Please do it manually.\n" +
                     "Error Code: 3\n" +
                     e.getMessage());
             e.printStackTrace();
         }
-
+        dialog.setProgress(100);
+        Thread.sleep(2000);
+        dialog.setVisible(false);
+        System.exit(0);
     }
 
     private static void execute(final String exec) throws IOException {
-        final Process p = Runtime.getRuntime().exec(exec);
-        System.out.println("Execute " + exec);
-        try {
-            System.out.println(p.waitFor());
-        } catch (final InterruptedException ignored) {
-        }
+        Runtime.getRuntime().exec(exec);
+        dialog.message("Execute " + exec);
     }
 
     private static void downloadAndExtract(final String url) throws IOException {
         final URL u = new URL(url);
         final URLConnection con = u.openConnection();
-        System.out.println("Downloading " + url);
+        dialog.message("Downloading " + url);
 
         final File outputFolder = new File(".");
 
         try (ZipInputStream zis = new ZipInputStream(con.getInputStream())) {
+
             ZipEntry e = zis.getNextEntry();
             while (e != null) {
                 if (!e.isDirectory()) {
                     final String zipFileName = e.getName();
                     final File zipNewFile = new File(outputFolder, zipFileName);
-                    System.out.println("Extracting " + zipFileName + " to " + zipNewFile);
+                    dialog.message("Extracting " + zipFileName + " to " + zipNewFile);
                     if (!new File(zipNewFile.getParent()).mkdirs()) {
-                        System.err.println("Could not make folder " + zipNewFile.getParent());
+                        dialog.message("Could not make folder " + zipNewFile.getParent());
                     }
                     try (FileOutputStream fos = new FileOutputStream(zipNewFile)) {
                         int len;
@@ -90,19 +99,5 @@ public final class UpdaterMain {
                 e = zis.getNextEntry();
             }
         }
-//        try (InputStream is = con.getInputStream(); FileOutputStream fos = new FileOutputStream(fileName)) {
-//            final byte[] data = new byte[1024];
-//            int n;
-//            while ((n = is.read(data)) != -1) {
-//                fos.write(data, 0, n);
-//            }
-//        }
-    }
-
-
-    private static void abort(final String message) {
-        System.err.println(message);
-        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
-        System.exit(1);
     }
 }
